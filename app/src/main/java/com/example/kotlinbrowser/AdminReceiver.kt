a2a46f9825a884557a4a75f90254652d60560d05
Jhoni Dewa <jhoni.dewa@gmail.com>
2021-03-25 14:50:32 +0100

    lock task is working if the app is set to device owner app backbutton is back function for the website url is read from external folder

--- app/src/main/java/com/example/kotlinbrowser/AdminReceiver.kt
+++ app/src/main/java/com/example/kotlinbrowser/AdminReceiver.kt
@@ -1,26 +1,29 @@
 package com.example.kotlinbrowser
 
 import android.app.admin.DeviceAdminReceiver
+import android.app.admin.DevicePolicyManager
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.UserManager
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class AdminReceiver : DeviceAdminReceiver() {
     companion object {
         fun getComponentName(context: Context): ComponentName {
             return ComponentName(context.applicationContext, AdminReceiver::class.java)
         }
-
         private val TAG = AdminReceiver::class.java.simpleName
     }
 
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onLockTaskModeEntering(context: Context, intent: Intent, pkg: String) {
         super.onLockTaskModeEntering(context, intent, pkg)
-        /*Log.d(TAG, "onLockTaskModeEntering")*/
     }
 
     override fun onLockTaskModeExiting(context: Context, intent: Intent) {
         super.onLockTaskModeExiting(context, intent)
-        /*Log.d(TAG, "onLockTaskModeExiting")*/
     }
 }
